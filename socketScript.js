6c654b8dbd052d9d6f9c6bd33236930ce1a57c85
Veeresh Nete <kneteviresh@gmail.com>
2023-11-13 19:35:14 +0530

    Update socketScript.js

--- socketScript.js
+++ socketScript.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 function connect(){
   stompClient = Stomp.client("wss://test.payworks.io/v1/cloudpos/websocket");
   stompClient.connect({
-    "serialNumber" : Math.trunc(Math.random()*.1000000);
+    "serialNumber" : Math.trunc(Math.random()*.1000000)
   }, function(frame){
     console.log("connected: " + frame);
   });
