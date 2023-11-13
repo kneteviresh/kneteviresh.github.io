33628f8f7f97d545e24fba98814a1182df4fba66
Veeresh Nete <kneteviresh@gmail.com>
2023-11-13 19:26:28 +0530

    Create socketScript.js

---
+++ socketScript.js
@@ -0,0 +1,15 @@
+var stompClient = null;
+
+$(document).ready(function(){
+  console.log("index page is ready");
+  connect();
+});
+
+function connect(){
+  stompClient = Stomp.client("wss://test.payworks.io/v1/cloudpos/websocket");
+  stompClient.connect({
+    "serialNumber" : Math.trunc(Math.random()*.1000000);
+  }, function(frame){
+    console.log("connected: " + frame);
+  });
+}
